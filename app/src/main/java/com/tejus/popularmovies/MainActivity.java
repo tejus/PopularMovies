c3ed56ca1a5d4562f5831aec4c2d95108284518f
Tejus Kandachar <tejus.k.v@gmail.com>
2019-01-24 14:59:56 +0530

    Add basic layout to preview data

--- app/src/main/java/com/tejus/popularmovies/MainActivity.java
+++ app/src/main/java/com/tejus/popularmovies/MainActivity.java
@@ -2,12 +2,23 @@ package com.tejus.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mDefaultTV;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDefaultTV = findViewById(R.id.tv_movies);
+        mProgressBar = findViewById(R.id.progress_loading);
+
+        mDefaultTV.setText("Sample Text");
     }
 }
