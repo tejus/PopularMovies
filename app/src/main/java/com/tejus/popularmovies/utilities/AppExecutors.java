718878ead9380c9cef6045ee00e97c2d2f8f1d66
Tejus Kandachar <tejus.k.v@gmail.com>
2019-02-20 17:52:05 +0530

    Add AppExecutors for networkIO and main thread

---
+++ app/src/main/java/com/tejus/popularmovies/utilities/AppExecutors.java
@@ -0,0 +1,49 @@
+package com.tejus.popularmovies.utilities;
+
+
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class AppExecutors {
+
+    private static final Object LOCK = new Object();
+    private static AppExecutors sInstance;
+    private final Executor mainThread;
+    private final Executor networkIO;
+
+    private AppExecutors(Executor networkIO, Executor mainThread) {
+        this.networkIO = networkIO;
+        this.mainThread = mainThread;
+    }
+
+    public static AppExecutors getInstance() {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = new AppExecutors(Executors.newFixedThreadPool(3),
+                        new MainThreadExecutor());
+            }
+        }
+        return sInstance;
+    }
+
+    public Executor mainThread() {
+        return mainThread;
+    }
+
+    public Executor networkIO() {
+        return networkIO;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
+}
