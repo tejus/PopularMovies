e6f8537aca7e06d1826b4190feee6919b1d6d967
Tejus Kandachar <tejus.k.v@gmail.com>
2019-02-20 17:53:25 +0530

    Add logging to RetrofitUtils

--- app/src/main/java/com/tejus/popularmovies/utilities/RetrofitUtils.java
+++ app/src/main/java/com/tejus/popularmovies/utilities/RetrofitUtils.java
@@ -1,5 +1,7 @@
 package com.tejus.popularmovies.utilities;
 
+import android.util.Log;
+
 import com.tejus.popularmovies.model.MovieResult;
 
 import java.io.IOException;
@@ -10,11 +12,14 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitUtils {
+
+    private static final String LOG_TAG = RetrofitUtils.class.getSimpleName();
+
     private static final String BASE_URL = "https://api.themoviedb.org/";
     private static final String LANGUAGE_KEY = "en-US";
+    private static MovieResult result = null;
 
     public static MovieResult fetchMovies(String sortMode, String apiKey) {
-        MovieResult result = null;
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -26,12 +31,15 @@ public class RetrofitUtils {
                 LANGUAGE_KEY,
                 Integer.toString(1));
 
+        Log.d(LOG_TAG, "Executing network call");
         try {
             Response<MovieResult> response = call.execute();
             result = response.body();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        Log.d(LOG_TAG, "Returning result from network call");
         return result;
     }
 }
