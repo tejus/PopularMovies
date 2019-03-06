e774e71565f206daa065c4f000e72da140cea98f
Tejus Kandachar <tejus.k.v@gmail.com>
2019-03-06 23:04:14 +0530

    Disable swiping left/right in ViewPager https://stackoverflow.com/questions/9650265

---
+++ app/src/main/java/com/tejus/popularmovies/ui/NoSwipeViewPager.java
@@ -0,0 +1,64 @@
+/*
+ ** Authors: louielouie & jungledev
+ ** Link: https://stackoverflow.com/questions/9650265
+ */
+package com.tejus.popularmovies.ui;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+
+import java.lang.reflect.Field;
+
+public class NoSwipeViewPager extends ViewPager {
+    public NoSwipeViewPager(@NonNull Context context) {
+        super(context);
+        setMyScroller();
+    }
+
+    public NoSwipeViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setMyScroller();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    private void setMyScroller() {
+        try {
+            Class<?> viewPager = ViewPager.class;
+            Field scroller = viewPager.getDeclaredField("mScroller");
+            scroller.setAccessible(true);
+            scroller.set(this, new MyScroller(getContext()));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class MyScroller extends Scroller {
+
+        private MyScroller(Context context) {
+            super(context, new DecelerateInterpolator());
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            super.startScroll(startX, startY, dx, dy, 150);
+        }
+    }
+
+}
